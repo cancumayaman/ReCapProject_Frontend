33d5759241fe4fa3a03466670acc840503684085
CanCumaYaman <65663050+CanCumaYaman@users.noreply.github.com>
2021-03-11 18:10:41 +0300

    Angular

---
+++ src/app/components/car/car.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-car',
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.css']
+})
+export class CarComponent implements OnInit {
+
+  car1={Id:1,Name:"Clio",Brand:"Renault",Price:100000}
+  car2={Id:2,Name:"Polo",Brand:"Wolsvagen",Price:200000}
+  car3={Id:3,Name:"Fiesta",Brand:"Ford",Price:50000}
+  cars=[
+    this.car1,this.car2,this.car3
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
